43074001a6daae4f58aad3104f288ea5448c4096
MohsinAli <mohammadmohsin@gmail.com>
2022-11-07 10:12:52 +0500

    Volunteers form - formatting issue.

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -4,7 +4,7 @@ class Volunteer < ApplicationRecord
   AREA_OF_INTEREST      = YAML.load_file( 'lib/data/data.yml' )["area_of_interest"]
   MARKETING_MEDIUM      = YAML.load_file( 'lib/data/data.yml' )["marketing_medium"]
 
-  validates :name, presence: true
+  validates :name, presence: { message: "Name can't be blank."}
   validates :email, presence: { message: 'Please enter a valid email address.' }
 
 end

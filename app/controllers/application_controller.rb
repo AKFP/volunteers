42aa125218be6f426b8d1b2200e1fb263e472168
MohsinAli <mohammadmohsin@gmail.com>
2022-11-12 11:08:33 +0500

    Redirect to login after logout.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     return volunteers_path
   end
+
+  def after_sign_out_path_for(resource)
+    return new_user_session_path
+  end
 end

8efa2ad67e6afb7b653ab8ef8e4e2818e27a9f38
MohsinAli <mohammadmohsin@gmail.com>
2022-11-16 08:48:08 +0500

    After signin, redirect admin to volunteers management.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,11 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    return volunteers_path
+    if resource.has_role?(:super_admin)
+      admin_volunteers_path()
+    else
+      return volunteers_path()
+    end
   end
 
   def after_sign_out_path_for(resource)

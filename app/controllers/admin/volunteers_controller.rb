d4f08579ddde77500a03c3494e215adf85ceaa9f
MohsinAli <mohammadmohsin@gmail.com>
2022-11-30 01:42:11 +0500

    Total count in tabs.

--- app/controllers/admin/volunteers_controller.rb
+++ app/controllers/admin/volunteers_controller.rb
@@ -9,10 +9,12 @@ class Admin::VolunteersController < AdminController
   end
 
   def index
-    @q = Volunteer.ransack(params[:q])
-    @volunteers = @q.result.registered.page(params[:registered])
-    @approved_volunteers = Volunteer.approved.page(params[:approved])
-    @rejected_volunteers = Volunteer.rejected.page(params[:rejected])
+    @q                    = Volunteer.ransack(params[:q])
+    @volunteers           = @q.result.registered.page(params[:registered])
+    @approved_volunteers  = Volunteer.approved.page(params[:approved])
+    @rejected_volunteers  = Volunteer.rejected.page(params[:rejected])
+    @ambassadors          = Volunteer.where(id: User.with_role(:ambassador).pluck(:id)).page(params[:ambassador])
+
     authorize! :manage, Volunteer, message: 'You are not authorized to access this page.'
   end
 

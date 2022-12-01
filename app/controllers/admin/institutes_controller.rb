5024568148c98fbc873103bbeb00bad237eeecfc
MohsinAli <mohammadmohsin@gmail.com>
2022-12-02 00:03:10 +0500

    Institute crud - Show & Edit

--- app/controllers/admin/institutes_controller.rb
+++ app/controllers/admin/institutes_controller.rb
@@ -1,5 +1,5 @@
 class Admin::InstitutesController < AdminController
-  # before_action :set_volunteer
+  before_action :set_institute, only: [:edit, :show, :update]
 
   def index
     @institutes = Institute.all
@@ -22,9 +22,26 @@ class Admin::InstitutesController < AdminController
     end
   end
 
+  def show; end
+
+  def edit; end
+
+  def update
+    respond_to do |format|
+      if @institute.update(institute_params)
+        format.html { redirect_to admin_institute_url(@institute), notice: "Institute was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
 
   private
   def institute_params
     params.require(:institute).permit(:name, :city_id, :address)
+  end
+
+  def set_institute
+    @institute = Institute.find_by(id: params[:id])
   end
 end

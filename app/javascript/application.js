fdb6bbd7c3992bf73e3ee066e2d24fe859649fbf
MohsinAli <mohammadmohsin@gmail.com>
2022-12-11 00:43:44 +0500

    Bucket name typo fixed.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
+import Bloodhound from "./typeahead"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./bootstrap-tagsinput"
--- app/javascript/controllers/volunteers_controller.js
+++ app/javascript/controllers/volunteers_controller.js
@@ -4,8 +4,31 @@ export default class extends Controller {
   static targets = ["name"];
 
   connect() {
-    console.log("Volunteers Controller ....");
-    // this.element.textContent = "From JS";
+    console.log("Volunteers Controller ...");
+    // var citynames = new Bloodhound({
+    //   datumTokenizer: Bloodhound.tokenizers.obj.whitespace('name'),
+    //   queryTokenizer: Bloodhound.tokenizers.whitespace,
+    //   prefetch: {
+    //     url: 'assets/citynames.json',
+    //     filter: function(list) {
+    //       return $.map(list, function(cityname) {
+    //         return { name: cityname }; });
+    //     }
+    //   }
+    // });
+    //
+    // debugger;
+
+    // citynames.initialize();
+    // $('.tginput').tagsinput({
+    //   typeaheadjs: {
+    //     name: 'citynames',
+    //     displayKey: 'name',
+    //     valueKey: 'name',
+    //     source: ["Karachi", "Lahore", "Islamabad"]
+    //   }
+    // });
+
   }
 
   updateStatus() {

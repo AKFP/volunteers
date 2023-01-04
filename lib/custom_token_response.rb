902fd143d46a0494236aa15ea1cc1faa82094079
Muhammad Tahir Saleh <abumutahira@gmail.com>
2023-01-04 13:51:30 +0500

    user apis volunteer name included

--- lib/custom_token_response.rb
+++ lib/custom_token_response.rb
@@ -5,8 +5,9 @@ module CustomTokenResponse
       # use any global storage like RequestStore / Rails Current / etc
       'email' => u.email,
       'user_id' => @token.resource_owner_id, # you have an access to the @token object
-      'volunteer_id' => u.volunteer.try(:id),
-      'volunteer_complete' => u.volunteer.try(:complete?)
+      'volunteer_name' => u.volunteer.try(:name), # volunteer name
+      'volunteer_id' => u.volunteer.try(:id), # volunteer id
+      'volunteer_complete' => u.volunteer.try(:complete?) # is volunteer information complete
     }
 
     # call original `#body` method and merge its result with the additional data hash

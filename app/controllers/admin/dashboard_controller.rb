e14df9aaa80e8d148ddc3cdbfccc869091483100
mohsinali <mohammadmohsin@gmail.com>
2023-03-03 11:12:58 +0500

    VDM dashboard stats for registered volunteers.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < AdminController
   def index
-
+    @last_24_hours  = Volunteer.where("created_at >= ?", Date.today - 24.hours).size
+    @last_week      = Volunteer.where("created_at >= ?", Date.today - 7.days).size
+    @last_month     = Volunteer.where("created_at >= ?", Date.today - 1.month).size
   end
 end

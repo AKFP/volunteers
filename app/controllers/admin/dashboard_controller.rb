ea5124d74ad235db4e66494e1b71796d6d4d19c2
mohsinali <mohammadmohsin@gmail.com>
2023-03-04 15:37:16 +0500

    Volunteers count by region and by city.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,17 @@
 class Admin::DashboardController < AdminController
   def index
+    @regions = Region.all
+    selected_cities = [56,66,88,90,29]
+    @cities_count = {}
+    @volunteers_by_city = Volunteer.where(city_id: selected_cities)
     @last_24_hours  = Volunteer.where("created_at >= ?", Date.today - 24.hours).size
     @last_week      = Volunteer.where("created_at >= ?", Date.today - 7.days).size
     @last_month     = Volunteer.where("created_at >= ?", Date.today - 1.month).size
+
+    selected_cities.each do |c|
+      volunteers = @volunteers_by_city.select{|v| v.city_id == c}
+      @cities_count[volunteers.first.city.name.downcase.to_sym] = volunteers.size
+    end
+
   end
 end
